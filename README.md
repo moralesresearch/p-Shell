06bb057c558ceb42e45d99d9512a33a986e9b0fc
Abdon Morales <abdon.morales@moralesresearch.org>
2023-08-06 14:51:01 -0500

    Adding the needed implementations and commands to the new p-Shell

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# p-Shell v3.3
+# p-Shell v3.3.1
 p-Shell is a Unix-compatible and UCSD p-System compatible Pascal Shell or short p-Shell
 
 Version 3.3 and below are based on Unix terminal command while 3.3.1 are more advanced and rely on Pascal itself rather than the operating system.
--- p-shell.pas
+++ p-shell.pas
@@ -1,134 +1,395 @@
 {$mode objfpc}{$H+}{$J-} // Just use this line in all modern sources
-program shell;
-uses BaseUnix, Unix, strings, sysutils; // Using sysutils over crt
+program p-Shell;
 
-type strArr = array[1..200] of string;
+uses
+  SysUtils, Crt;
 
-procedure startup;
+const
+  Version = '3.3.1'; // Set the shell version number here
+  ShellName = 'p-Shell'; // Set the shell name here
+  MaxPathLength = 255;
+  MaxCommandLength = 50;
+
+var
+  CurrentPath: string;
+  Command: string;
+  CommandParams: array [1..5] of string;
+  NumParams: Integer;
+
+procedure DisplayCompatibilityWarning;
 begin
-	unix.fpsystem('clear');
-	Write (sLineBreak + ' Pascal Disk Operating System v3.3 ');
-	Write (sLineBreak + ' August 2023 ');
-	Write (sLineBreak + '(C) 2023 Morales Research Inc');
-	//Delay(4000);
-	Sleep(5000); // The currect fixed solution and replacement for Delay and Crt
-	unix.fpsystem('clear');
-	Write (sLineBreak + 'p-DOS 3.3 - Aug 2023');
-	Write (sLineBreak + ' p-Shell v3.3 ' + sLineBreak);
-	Sleep(2500);
-	Write (' READY! ');
+  WriteLn('Warning: The current command is not supported in older versions of ', ShellName);
+  WriteLn('Please consider upgrading to the latest version (', Version, ') to use all features.');
+  WriteLn;
 end;
 
-procedure prompt;
-var i : integer;
+procedure DisplayCopyrightNotice;
 begin
+  WriteLn(ShellName, ' v', Version);
+  WriteLn;
+  WriteLn('(C) 1979 Apple Inc. All rights reserved.');
+  WriteLn('(C) 2023 Morales Research Inc. All rights reserved.');
+  WriteLn;
+end;
 
-	writeln;
-  i := BaseUnix.FpGetuid;
-  if i = 0 then begin 
-    write ('# ')
-  end
-  else begin
-    write ('p-Shell> ');
-  end; //if
-end; //prompt
-
-procedure inLine(var l: string);
+function GetScreenWidth: Integer;
 begin
-  readln(l);
-end; //inLine
+  // On Linux, we cannot get the screen width directly.
+  // You may implement a cross-platform solution using Crt unit.
+  // For this demonstration, we'll return a default value.
+  Result := 80;
+end;
 
-procedure getSeperateCommands(var str0 : string; var seperated : strArr; var argCount : integer;var ampersand : boolean );
-var i,j : integer;
-	currStr : string;
+procedure Initialize;
 begin
-	i := 1;
-	j := 1;
+  CurrentPath := '/';
+end;
 
-	while i <> (length(str0) + 1) do
-	begin
-		if str0[i] <> ' ' then
-		begin
-			currStr := currStr + str0[i];
-			if (str0[i+1] = ' ') OR ((i+1) = (length(str0) + 1)) then
-			begin
-				seperated[j] := currStr;
-				inc(j);
-				currStr := '';
-			end;
-		end;
-		inc(i);
-	end;
-	argCount := j-1;
-
-	if seperated[argcount] = '&' then
-		begin
-			seperated[argCount] := '';
-			argCount := argCount - 1;
-			ampersand := true;
-		end;
-
-end; //getSeperateCommands
-
-procedure run(var commands: strArr; var argCount : integer);
+procedure ParseCommand;
 var
-  PP : PPchar;
-  P0 : PChar;
-  i : integer;
+  CommandStr: string;
+  ParamCount: Integer;
 begin
-	i:=1;
-	GetMem(PP,argCount*SizeOf(Pchar));
+  Write('': (GetScreenWidth - Length(ShellName + ' v' + Version)) div 2);
+  WriteLn(ShellName, ' v', Version); // Display centered version and name
 
-	while i <= argCount do
-	begin
-		P0 := StrAlloc(length(commands[i])+1);
-		StrPCopy(P0, commands[i]);
-		PP[i-1] := P0;
-		inc(i);
-	end;
+  Write(ShellName, ' v', Version, ' >> '); // Display version and name
+  ReadLn(CommandStr);
 
-	PP[i-1] := Nil;
-	Unix.FpExecVP(commands[1], PP);
+  ParamCount := 0;
+  while Length(CommandStr) > 0 do
+  begin
+    Inc(ParamCount);
+    CommandParams[ParamCount] := Trim(ExtractWord(1, CommandStr, [' ']));
+  end;
 
-end; //run
+  Command := CommandParams[1];
+  NumParams := ParamCount - 1;
+end;
 
-procedure main;
-var
-  ampersand : boolean;
-  ln : string;
-   argCount : integer;
-  commands : strArr;
-  pid : longint;
+procedure DisplayVersion;
 begin
-  ampersand := false;
-  
-  // startup;
-  prompt;
-  inLine(ln);
+  WriteLn('Shell Name: ', ShellName);
+  WriteLn('Shell Version: ', Version);
+  // Display the copyright notice when "version" command is executed
+  DisplayCopyrightNotice;
+  {$IFDEF MSWINDOWS}
+    WriteLn('Operating System: Windows');
+  {$ELSE}
+    WriteLn('Operating System: Unix'); // Since this is a Unix-compatible p-Shell
+  {$ENDIF}
+end;
 
-  getSeperateCommands(ln, commands, argCount, ampersand);
-
-  pid := BaseUnix.FpFork;
-
-  if pid = 0 then
-  	begin
-    	run(commands, argCount);
-  	end
+procedure ExecuteCommand;
+begin
+  case Command of
+    'cd': ChangeDirectory;
+    'ls': ListDirectory;
+    'copy': CopyFile;
+    'cat': DisplayFileContents;
+    'shutdown': ShutdownSystem;
+    'reboot': RebootSystem;
+    'logout': Logout;
+    'version': DisplayVersion;
+    'tree': DisplayFileTree;
+    'memchk': MemoryCheck;
+    'clear': ClearScreen;
+    'pascal': ExecutePascalInterpreter;
+    'exit': Exit; // Command to exit back to the p-Shell
   else
-  	begin
-		if ampersand = false then
-			begin
-				FpWait(pid);
-			end
-  	end;
-main;
+    begin
+      // Check for older versions before executing certain commands
+      if (Command = 'grep') or (Command = 'diskcopy') then
+        DisplayCompatibilityWarning
+      else
+        WriteLn('Command not recognized: ', Command);
+    end;
+  end;
 end;
 
+procedure ChangeDirectory;
+begin
+  if NumParams >= 1 then
+  begin
+    if DirectoryExists(CommandParams[1]) then
+    begin
+      CurrentPath := IncludeTrailingPathDelimiter(CommandParams[1]);
+    end
+    else
+    begin
+      WriteLn('Directory not found: ', CommandParams[1]);
+    end;
+  end
+  else
+  begin
+    WriteLn('Usage: cd <directory>');
+  end;
+end;
 
+procedure ListDirectory;
+var
+  SearchRec: TSearchRec;
+  FindResult: Integer;
+begin
+  FindResult := FindFirst(CurrentPath + '*', faAnyFile, SearchRec);
+  if FindResult = 0 then
+  begin
+    repeat
+      WriteLn(SearchRec.Name);
+    until FindNext(SearchRec) <> 0;
+    FindClose(SearchRec);
+  end;
+end;
 
+procedure CopyFile;
+begin
+  if NumParams >= 2 then
+  begin
+    if FileExists(CommandParams[1]) then
+    begin
+      if not FileExists(CommandParams[2]) then
+      begin
+        FileCopy(CommandParams[1], CommandParams[2]);
+        WriteLn('File copied successfully.');
+      end
+      else
+      begin
+        WriteLn('Destination file already exists: ', CommandParams[2]);
+      end;
+    end
+    else
+    begin
+      WriteLn('Source file not found: ', CommandParams[1]);
+    end;
+  end
+  else
+  begin
+    WriteLn('Usage: copy <source> <destination>');
+  end;
+end;
+
+procedure DisplayFileContents;
+var
+  FileHandle: TextFile;
+  Line: string;
+begin
+  if NumParams >= 1 then
+  begin
+    if FileExists(CommandParams[1]) then
+    begin
+      Assign(FileHandle, CommandParams[1]);
+      Reset(FileHandle);
+      while not Eof(FileHandle) do
+      begin
+        ReadLn(FileHandle, Line);
+        WriteLn(Line);
+      end;
+      Close(FileHandle);
+    end
+    else
+    begin
+      WriteLn('File not found: ', CommandParams[1]);
+    end;
+  end
+  else
+  begin
+    WriteLn('Usage: cat <file>');
+  end;
+end;
+
+procedure SearchFileContents;
+var
+  FileHandle: TextFile;
+  Line: string;
+  SearchPattern: string;
+begin
+  if NumParams >= 2 then
+  begin
+    SearchPattern := CommandParams[1];
+    if FileExists(CommandParams[2]) then
+    begin
+      Assign(FileHandle, CommandParams[2]);
+      Reset(FileHandle);
+      while not Eof(FileHandle) do
+      begin
+        ReadLn(FileHandle, Line);
+        if Pos(SearchPattern, Line) > 0 then
+        begin
+          WriteLn(Line);
+        end;
+      end;
+      Close(FileHandle);
+    end
+    else
+    begin
+      WriteLn('File not found: ', CommandParams[2]);
+    end;
+  end
+  else
+  begin
+    WriteLn('Usage: grep <search pattern> <file>');
+  end;
+end;
+
+procedure ShutdownSystem;
+begin
+  {$IFDEF MSWINDOWS}
+    WriteLn('Shutting down Windows system...');
+    // Use the appropriate OS command or API to shutdown the Windows system
+  {$ELSE}
+    WriteLn('Shutting down Unix system...');
+    // Use the appropriate Unix OS command or API to shutdown the system
+    {$IFDEF UNIX}
+      SysUtils.ExecuteProcess('/sbin/shutdown', '-h now', []);
+    {$ELSE}
+      WriteLn('Shutdown command is not supported in this OS.');
+    {$ENDIF}
+  {$ENDIF}
+end;
+
+procedure RebootSystem;
+begin
+  {$IFDEF MSWINDOWS}
+    WriteLn('Rebooting Windows system...');
+    // Use the appropriate OS command or API to reboot the Windows system
+  {$ELSE}
+    WriteLn('Rebooting Unix system...');
+    // Use the appropriate Unix OS command or API to reboot the system
+    {$IFDEF UNIX}
+      SysUtils.ExecuteProcess('/sbin/reboot', '', []);
+    {$ELSE}
+      WriteLn('Reboot command is not supported in this OS.');
+    {$ENDIF}
+  {$ENDIF}
+end;
+
+procedure Logout;
+begin
+  {$IFDEF MSWINDOWS}
+    WriteLn('Logging out from Windows...');
+    // Implement user logout logic here if applicable for Windows
+  {$ELSE}
+    WriteLn('Logging out from Unix...');
+    // Use the appropriate Unix OS command or API to logout the user
+    {$IFDEF UNIX}
+      SysUtils.ExecuteProcess('/usr/bin/logout', '', []);
+    {$ELSE}
+      WriteLn('Logout command is not supported in this OS.');
+    {$ENDIF}
+  {$ENDIF}
+end;
+
+procedure DisplayFileTree;
+begin
+  {$IFDEF MSWINDOWS}
+    WriteLn('Displaying file tree is not supported in Windows.');
+  {$ELSE}
+    WriteLn('Displaying file tree in Unix...');
+    // Use the appropriate Unix OS command or API to display the file tree
+    {$IFDEF UNIX}
+      SysUtils.ExecuteProcess('/usr/bin/tree', CurrentPath, []);
+    {$ELSE}
+      WriteLn('Displaying file tree command is not supported in this OS.');
+    {$ENDIF}
+  {$ENDIF}
+end;
+
+procedure MemoryCheck;
+begin
+  {$IFDEF MSWINDOWS}
+    WriteLn('Checking memory usage in Windows...');
+    // Use the appropriate OS command or API to check memory usage in Windows
+  {$ELSE}
+    WriteLn('Checking memory usage in Unix...');
+    // Use the appropriate Unix OS command or API to check memory usage
+    {$IFDEF UNIX}
+      SysUtils.ExecuteProcess('/usr/bin/free', '-h', []);
+    {$ELSE}
+      WriteLn('Memory check command is not supported in this OS.');
+    {$ENDIF}
+  {$ENDIF}
+end
+
+procedure ClearScreen;
+begin
+  // Implement the clear screen command here
+  ClrScr;
+end;
+
+procedure DiskCopy;
+begin
+  {$IFDEF MSWINDOWS}
+    WriteLn('Disk copy is not supported in Windows.');
+  {$ELSE}
+    WriteLn('Copying disks in Unix...');
+    // Use the appropriate Unix OS command or API to perform disk copying
+    {$IFDEF UNIX}
+      if NumParams >= 2 then
+      begin
+        SysUtils.ExecuteProcess('/bin/dd', 'if=' + CommandParams[1] + ' of=' + CommandParams[2], []);
+      end
+      else
+      begin
+        WriteLn('Usage: diskcopy <source> <destination>');
+      end;
+    {$ELSE}
+      WriteLn('Disk copy command is not supported in this OS.');
+    {$ENDIF}
+  {$ENDIF}
+end;
+
+procedure ExecutePascalInterpreter;
+var
+  PascalCode: TStringList;
+  TempPascalFile: string;
+  ExecutionResult: Integer;
+begin
+  PascalCode := TStringList.Create;
+  try
+    WriteLn('Enter your Pascal code. Type "##" on a new line and press Enter to finish:');
+    repeat
+      PascalCode.Add(ReadLn);
+    until PascalCode[PascalCode.Count - 1] = '##';
+
+    TempPascalFile := GetCurrentDir + PathDelim + 'temp.pas';
+    PascalCode.SaveToFile(TempPascalFile);
+
+    WriteLn('Executing ', PascalInterpreterName, ' v', PascalInterpreterVersion, '...');
+    ExecutionResult := SysUtils.ExecuteProcess('/usr/bin/fpc', TempPascalFile, []);
+    if ExecutionResult = 0 then
+    begin
+      WriteLn(PascalInterpreterName, ' v', PascalInterpreterVersion, ': Pascal code executed successfully.');
+      SysUtils.ExecuteProcess('./temp', '', []); // Execute the compiled program
+    end
+    else
+    begin
+      WriteLn(PascalInterpreterName, ' v', PascalInterpreterVersion, ': Pascal code execution failed.');
+    end;
+
+    PascalCode.Clear;
+    DeleteFile(TempPascalFile);
+  finally
+    PascalCode.Free;
+  end;
+end;
 
 begin
-  startup;
-  main;
+  ClrScr;
+  DisplayCopyrightNotice; // Display copyright notice at startup
+  Initialize;
 
+  while True do
+  begin
+    ParseCommand;
+    if Command = 'exit' then
+      Break
+    else if Command = 'version' then
+    begin
+      DisplayVersion; // Execute DisplayVersion procedure for the "version" command
+    end
+    else
+      ExecuteCommand;
+  end;
+
+  WriteLn('Exiting ', ShellName);
 end.
